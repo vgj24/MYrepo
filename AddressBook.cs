29f0c94feea4cbcaaee22e3c826fbb07a2c0d0bf
vgj24 <vrushlijoshi24@gmail.com>
2022-02-16 10:36:21 +0530

    commiting to branch b1

---
+++ AddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject10FebAssgn
+{
+    internal class AddressBook
+    {
+        // UC1
+        public static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
+            listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", 17));
+            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", 35));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
+            listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
+            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
+            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+             Console.WriteLine(listPersonInCity.ToString());
+            listPersonInCity.ForEach(x => Console.WriteLine("{0}\t",x.Name.ToString()));
+
+        }
+    }
+}
